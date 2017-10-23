d91cbdc2fa789b1a57efe25f66a21eebc64ab616
seven.l <ititonight@gmail.com>
2017-10-23 16:42:20 +0800

    - delete useless files

--- client/src/config/development.js
+++ client/src/config/development.js
@@ -1,4 +1,3 @@
 module.exports = {
-    API_HOST: 'https://tapi.star0ad.com'
-    // API_HOST: 'http://172.16.50.52:8083'
+
 }
--- client/src/config/production.js
+++ client/src/config/production.js
@@ -1,4 +1,2 @@
 module.exports = {
-    // API_HOST: 'http://172.16.50.52:8083'
-    API_HOST: 'https://tapi.star0ad.com'
 }
